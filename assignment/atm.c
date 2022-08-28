1560e58f3d4dc74d364187fdab0bbc4b46f13a0a
Theonlysmartboy <o2jose43@gmail.com>
2022-08-28 12:27:21 +0300

    added login

--- assignment/atm.c
+++ assignment/atm.c
@@ -5,7 +5,7 @@
 #include <math.h>
 
 // Functions
-void login();
+bool login(int pin);
 void mainMenu();
 void checkBalance(float balance);
 float moneyDeposit(float balance);
@@ -20,57 +20,67 @@ int main()
     int option;
     float balance = 15000.00;
     int choose;
+    int myPin;
 
     bool again = true;
 
     // insert code here...
+    printf("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+    printf("Enter Your Pin Number:\t");
+    scanf("%d", &myPin);
 
-    while (again)
+    if (login(myPin))
     {
-        mainMenu();
-
-        printf("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
-        printf("Your Selection:\t");
-        scanf("%d", &option);
-
-        switch (option)
+        while (again)
         {
-        case 1:
-            system("CLS");
-            checkBalance(balance);
-            break;
-        case 2:
-            system("CLS");
-            balance = moneyDeposit(balance);
-            break;
-        case 3:
-            system("CLS");
-            balance = moneyWithdraw(balance);
-            break;
+            mainMenu();
 
-        case 4:
-            system("CLS");
-            menuExit();
-            return 0;
+            printf("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+            printf("Your Selection:\t");
+            scanf("%d", &option);
 
-        default:
-            errorMessage();
-            break;
+            switch (option)
+            {
+            case 1:
+                system("CLS");
+                checkBalance(balance);
+                break;
+            case 2:
+                system("CLS");
+                balance = moneyDeposit(balance);
+                break;
+            case 3:
+                system("CLS");
+                balance = moneyWithdraw(balance);
+                break;
+
+            case 4:
+                system("CLS");
+                menuExit();
+                return 0;
+
+            default:
+                errorMessage();
+                break;
+            }
+
+            printf("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n");
+            printf("Would you like to peform another transaction:\n");
+            printf("< 1 > Yes\n");
+            printf("< 2 > No\n");
+            scanf("%d", &choose);
+
+            system("CLS");
+
+            if (choose == 2)
+            {
+                again = false;
+                menuExit();
+            }
         }
-
-        printf("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n");
-        printf("Would you like to peform another transaction:\n");
-        printf("< 1 > Yes\n");
-        printf("< 2 > No\n");
-        scanf("%d", &choose);
-
-        system("CLS");
-
-        if (choose == 2)
-        {
-            again = false;
-            menuExit();
-        }
+    }
+    else
+    {
     }
 
     return 0;
@@ -158,3 +168,17 @@ void errorMessage()
     ;
     printf("+++!!!You have selected an  invalid number!!!+++\n");
 } // error message
+
+bool login(int pin)
+{
+    int storedpPin = 12345678;
+    if (storedpPin == pin)
+    {
+        return true;
+    }
+    else
+    {
+        printf("+++!!!You have enterd an  invalid Pin number!!!+++\n");
+        return false;
+    }
+}
